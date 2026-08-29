4fbefaaba368791a70ffdc2cac2942a680b55b0b
Saul <marcos.castro@tecsup.edu.pe>
2026-08-28 20:40:28 -0500

    modificando StudentController

--- StudentController.java
+++ StudentController.java
@@ -2,4 +2,8 @@ public class StudentController {
     public void getStudent() {
         System.out.println("Obteniendo estudiante");
     }
+
+    public void deleteStudent() {
+        System.out.println("Eliminando estudiante");
+    }
 }
