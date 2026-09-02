7de0cc50f46ea28e14a328b6f24919ccab8fa54b
castrocarim63-crl <castrocarim63@gmail.com>
2026-09-02 12:35:02 -0500

    Agregada nueva funcionalidad del colaborador

--- src/main/java/com/tecsup/lab/StudentController.java
+++ src/main/java/com/tecsup/lab/StudentController.java
@@ -5,5 +5,7 @@ public class StudentController {
         System.out.println("StudentController - Versión Sprint-1");
         System.out.println("Modificado por Responsable en sprint-1");
         System.out.println("Nueva funcionalidad en desarrollo");
+        System.out.println("Nueva funcionalidad en desarrolloss");
+
     }
 }
