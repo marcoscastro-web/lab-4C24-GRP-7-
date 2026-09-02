c1a92374fd4270910a74f99a7ed316f55ac5eb89
Marcos Castro <marcos.castro@tecsup.edu.pe>
2026-09-01 22:06:48 -0500

    Primer commit: Proyecto base con StudentController

---
+++ src/main/java/com/tecsup/lab/StudentController.java
@@ -0,0 +1,7 @@
+package com.tecsup.lab;
+
+public class StudentController {
+    public static void main(String[] args) {
+        System.out.println("StudentController - Versión Inicial");
+    }
+}
