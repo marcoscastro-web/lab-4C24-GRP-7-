799ab163794cf291fe2760f6be7fa8bf9b65cf19
castrocarim63-ctrl <castrocarim63@gmail.com>
2026-09-02 00:33:26 -0500

    Modificación en StudentReport - Colaborador en sprint-1

--- src/main/java/com/tecsup/lab/StudentReport.java
+++ src/main/java/com/tecsup/lab/StudentReport.java
@@ -4,5 +4,6 @@ public class StudentReport {
     public static void generateReport() {
         System.out.println("Generando reporte de estudiantes...");
         System.out.println("Reporte generado exitosamente");
+        System.out.println("Versión: Sprint-1 - Modificado por Colaborador");
     }
 }
