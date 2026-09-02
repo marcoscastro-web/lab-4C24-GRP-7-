0e9212c8e075af86845c6ce4a1aa099ba222cbac
Marcos Castro <marcos.castro@tecsup.edu.pe>
2026-09-01 22:09:29 -0500

    Modificación en StudentController por Responsable

--- src/main/java/com/tecsup/lab/StudentController.java
+++ src/main/java/com/tecsup/lab/StudentController.java
@@ -2,6 +2,7 @@ package com.tecsup.lab;
 
 public class StudentController {
     public static void main(String[] args) {
-        System.out.println("StudentController - Versión Inicial");
+        System.out.println("StudentController - Versión Modificada por Responsable");
+        System.out.println("Nueva funcionalidad agregada");
     }
 }
