b3740afc36fd4923c1bd8223851a04c23fe1a47a
castrocarim63-ctrl <castrocarim63@gmail.com>
2026-09-01 22:38:19 -0500

    Creación de StudentReport por Colaborador

---
+++ src/main/java/com/tecsup/lab/StudentReport.java
@@ -0,0 +1,8 @@
+package com.tecsup.lab;
+
+public class StudentReport {
+    public static void generateReport() {
+        System.out.println("Generando reporte de estudiantes...");
+        System.out.println("Reporte generado exitosamente");
+    }
+}
